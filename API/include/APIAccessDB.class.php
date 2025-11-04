b9025518f96c2e100ee836ecac29cd46a9cfcd87
Pete OConnell <manchesteroc@gmail.com>
2025-11-04 19:01:29 +0000

    registration work

--- API/include/APIAccessDB.class.php
+++ API/include/APIAccessDB.class.php
@@ -80,7 +80,7 @@ class APIAccessDB
 	{
 		$dbh = $this->database->dbh;
 
-		$sth = $dbh->prepare("Select apikey, is_banned FROM apiusers where user_id = :user_id AND is_private_key = 0 LIMIT 1;");
+		$sth = $dbh->prepare("Select apikey, is_banned FROM apiusers where user_id = :users_id AND is_private_key = 0 LIMIT 1;");
 		$sth->bindValue(':user_id', $user_id, PDO::PARAM_INT);
 
 		if($sth->execute())
@@ -101,9 +101,9 @@ class APIAccessDB
 			{
 				$bytes = openssl_random_pseudo_bytes(64/2);
 				$key = bin2hex($bytes);
-				$sth = $dbh->prepare("INSERT INTO apiusers (user_id, apikey, api_allowance_level_id, extra_allowance, is_private_key)
-				VALUES(:user_id, :apikey, 1, 0, 0);");
-				$sth->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+				$sth = $dbh->prepare("INSERT INTO apiusers (users_id, apikey, api_allowance_level_id, extra_allowance, is_private_key)
+				VALUES(:users_id, :apikey, 1, 0, 0);");
+				$sth->bindValue(':users_id', $user_id, PDO::PARAM_INT);
 				$sth->bindValue(':apikey', $key, PDO::PARAM_INT);
 
 				if($sth->execute())
