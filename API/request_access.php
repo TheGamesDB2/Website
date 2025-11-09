a289447dd36d4eaeafd685abd67f160ab5268347
Pete OConnell <manchesteroc@gmail.com>
2025-11-09 18:18:25 +0000

    api requests

--- API/request_access.php
+++ API/request_access.php
@@ -173,37 +173,40 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         }
     }
 }
+?>
+<!DOCTYPE html>
+<html>
 
-// Include header
-require_once __DIR__ . "/../website/include/header.footer.class.php";
+<head>
+	<meta charset="utf-8" />
+	<title>TheGamesDB API DOCs</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<style>
+		body {
+			margin: 50px 0 0 0;
+			padding: 0;
+			width: 100%;
+			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			text-align: center;
+			color: #aaa;
+			font-size: 18px;
+		}
+		
+		h1 {
+			color: #719e40;
+			letter-spacing: -3px;
+			font-family: 'Lato', sans-serif;
+			font-size: 100px;
+			font-weight: 200;
+			margin-bottom: 0;
+		}
+	</style>
+</head>
 
-$Header = new HEADER();
-$Header->setTitle("TGDB - API Access Request");
-$Header->appendRawHeader(function() { global $tgdb_user; ?>
-    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
-    <!-- Explicit paths to CSS and JS files to avoid 404 errors -->
-    <link rel="stylesheet" href="<?= CommonUtils::$WEBSITE_BASE_URL ?>css/main.css" crossorigin="anonymous">
-    <script type="text/javascript">
-        // Ensure jQuery and Bootstrap are loaded
-        if (typeof jQuery === 'undefined') {
-            document.write('<script src="<?= CommonUtils::$WEBSITE_BASE_URL ?>js/jquery-3.2.1.min.js"><\/script>');
-        }
-        if (typeof $.fn.modal === 'undefined') {
-            document.write('<script src="<?= CommonUtils::$WEBSITE_BASE_URL ?>js/bootstrap.min.4.0.0.js"><\/script>');
-        }
-    </script>
-    <style>
-        h1 {
-            color: #719e40;
-            letter-spacing: -3px;
-            font-family: 'Lato', sans-serif;
-            font-size: 100px;
-            font-weight: 200;
-            margin-bottom: 0;
-        }
-    </style>
-<?php });?>
-<?= $Header->print(); ?>
 
 <div class="container">
     <h1 class="text-center">API Access Request</h1>
