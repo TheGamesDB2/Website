7ae9459b227a043619b11c3279d6fca73b4364e9
Pete OConnell <manchesteroc@gmail.com>
2025-12-17 22:34:59 +0000

    block IPs who hit with invalid api key

--- API/include/middleware.php
+++ API/include/middleware.php
@@ -70,7 +70,7 @@ class AuthMiddleware
 			}
 			else
 			{
-				$JSON_Response = Utils::getStatus(499);
+				$JSON_Response = Utils::getStatus(403);
 				$rateLimiter->block($ip);
 				$JSON_Response['remaining_monthly_allowance'] = 0;
 				$JSON_Response['allowance_refresh_timer'] = 0;
