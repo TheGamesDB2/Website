c978b82c168eb7bffd63a0b54ad1d95c6c8ece70
Pete OConnell <manchesteroc@gmail.com>
2025-12-17 22:12:05 +0000

    block IPs who hit with invalid api key

--- API/include/middleware.php
+++ API/include/middleware.php
@@ -28,6 +28,12 @@ class AuthMiddleware
 		}
 		else
 		{
+			if ($_REQUEST['apikey'] == '4d58c55c36fea098f60a359bfddbf461ce5f7e462dddda659b6dfcfce6b455d')
+			{
+				rateLimiter->block($ip);
+				$JSON_Response = Utils::getStatus(999);
+				return $response->withJson($JSON_Response, $JSON_Response['code']);
+			}
 			$auth = APIAccessDB::getInstance();
 			$User = $auth->GetUserAllowanceByAPIKey($_REQUEST['apikey']);
 			if(!empty($User))
